f226c7a49ba333a0ce06e9c8c6b5281131106c20
scrappy-cyborg-4243 <itchkimel@gmail.com>
2020-08-12 20:07:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 function add(a, b){
   return (a + b);
 }
@@ -28,5 +29,67 @@ function makeInt(n){
 
 function preserveDecimal(n){
   return parseFloat(n); 
+=======
+function add(){
+  1 + 80;
+}
+
+function subtract(){
+  60 - 40;
+}
+
+function multiply(){
+  2 * 3.4;
+}
+
+function divide(){
+  5.0 / 2.5;
+}
+
+var number = 10
+
+function add5() {
+  number += 5
+}
+
+function divideBy3() {
+  number /= 3
+}
+
+divideBy3()
+ 
+console.log(number)
+
+number = 10
+ 
+add5()
+ 
+console.log(number) // 15
+ 
+divideBy3()
+ 
+console.log(number)
+
+var n = 5;
+
+function increment(n){
+  n +=1;
+  return `${n}`;
+}
+
+function decrement(n){
+  n -=1;
+  return `${n}`;
+}
+
+function makeInt(string){
+  parseInt("string", 10); 
+  return `${string}`
+}
+
+function preserveDecimal(string){
+  parseFloat("string"); 
+  return `${float}`
+>>>>>>> fcee66d36ddf82582f6505fa976fe380499a31d3
 }
 
